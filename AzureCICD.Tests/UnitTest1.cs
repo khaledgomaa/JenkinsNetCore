a8cf3d49dcb8d087dcdc6f1c766f86200b32136a
khaledgomaa <36606053+khaledgomaa@users.noreply.github.com>
2022-03-11 16:15:20 +0200

    Init

---
+++ AzureCICD.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Xunit;
+
+namespace AzureCICD.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test_Data()
+        {
+            bool isValid = true;
+            Assert.True(isValid);
+        }
+    }
+}
